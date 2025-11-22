7fbec4856bae0372cb2f5e476392ad5a9973fa05
Popoy <pocutqanutah@gmail.com>
2025-11-22 12:35:46 +0700

    gg

--- src/pages/Akun.jsx
+++ src/pages/Akun.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Akun = () => {
   return (
-    <section className="w-full flex flex-col justify-center items-start h-screen bg-gradient-to-b from-white via-white/80 to-orange-200">
+    <section className="w-full flex flex-col justify-center items-start h-screen bg-linear-to-b from-white via-white/80 to-orange-200">
       <h1>akun akun akun</h1>
     </section>
   );
